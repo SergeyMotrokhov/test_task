31223169d436b3eb59d563061a9d6cad678c536d
admin <leshch2016@yandex.ru>
2021-03-21 21:50:52 +0300

    Revert "test commit"
    This reverts commit 66061e4e062a830824396f68d16820a15ae1eea6.

--- src/test/java/tests/GetSuperHeroTests.java
+++ src/test/java/tests/GetSuperHeroTests.java
@@ -12,7 +12,6 @@ public class GetSuperHeroTests {
     @Test
     public void getTest() {
         List<SuperHero> response = requestGetSuperHeroStep(200);
-        System.out.println("123");
     }
 
 //    @Test
